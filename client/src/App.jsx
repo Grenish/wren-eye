40caf50840121b58c14b22a42e109b9dcc6d2bf5
Grenish rai <ariestheracer@gmail.com>
2024-03-12 03:52:20 +0530

    Add App component with Interface and VoiceToTextInterface components

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import VoiceToTextInterface from './components/VoiceToTextInterface'
+import Interface from './components/Interface'
+
+const App = () => {
+  return (
+    <div>
+      {/* <VoiceToTextInterface /> */}
+      {/* <Dictaphone /> */}
+      <Interface />
+    </div>
+  )
+}
+
+export default App
