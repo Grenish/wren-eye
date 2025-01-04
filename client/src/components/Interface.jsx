700127d84a310464a39929c54e6c0794d860a1c1
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-01-05 02:39:12 +0530

    init

--- client/src/components/Interface.jsx
+++ client/src/components/Interface.jsx
@@ -1,27 +1,29 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
+
+const speechRecognition =
+  window.speechRecognition || window.webkitSpeechRecognition;
+const mic = speechRecognition ? new speechRecognition() : null;
+
+if (mic) {
+  mic.continuous = true;
+}
 
 const Interface = () => {
   const videoRef = useRef(null);
-  const [isCameraOn, setIsCameraOn] = useState(false);
-  let stream = null;
+  const [isListening, setIsListening] = useState(false);
+  const [text, setText] = useState("");
 
-  const toggleCamera = async () => {
-    if (!isCameraOn) {
+  useEffect(() => {
+    let stream = null;
+    const enableCamera = async () => {
       if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         stream = await navigator.mediaDevices.getUserMedia({ video: true });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
       }
-    } else {
-      if (stream) {
-        stream.getTracks().forEach((track) => track.stop());
-      }
-    }
-    setIsCameraOn(!isCameraOn);
-  };
-
-  useEffect(() => {
+    };
+    enableCamera();
     return () => {
       if (stream) {
         stream.getTracks().forEach((track) => track.stop());
@@ -29,26 +31,76 @@ const Interface = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (mic) {
+      mic.onstart = () => {
+        console.log("Mics in on. Listening...");
+      };
+      mic.onresult = (event) => {
+        const transcript = Array.from(event.results)
+          .map((results) => results[0])
+          .map((results) => results.transcript)
+          .join("");
+        setText(transcript);
+      };
+      mic.onend = () => {
+        console.log("Mics is off. Not listening...");
+      };
+    } else {
+      console.log("Your browser does not support speech recognition");
+    }
+  }, [isListening]);
+
+  const startListening = () => {
+    if (mic) {
+      setText("");
+      mic.start();
+      setIsListening(true);
+    }
+  };
+
+  const stopListening = () => {
+    if (mic) {
+      mic.stop();
+      setIsListening(false);
+    }
+  };
+
   return (
-    <div className="bg-slate-900 w-full h-screen flex flex-col justify-center items-center">
-      <div className="w-full m-auto h-full flex flex-col justify-center items-center">
-        <div className="w-1/2 ">
-          <button className="text-white" onClick={toggleCamera}>
-            {isCameraOn ? "Turn camera off" : "Turn camera on"}
-          </button>
-        </div>
-        <div className=" w-1/2 h-1/2 border rounded-xl overflow-hidden">
+    <div className="bg-slate-950 w-full h-screen flex flex-col justify-center items-center">
+      <div className="w-full m-auto h-full flex justify-center items-center">
+        <div className="w-1/2 h-2/3 border rounded-3xl overflow-hidden">
           <video
             ref={videoRef}
-            autoPlay
             className="w-full h-full object-cover"
+            autoPlay
           ></video>
         </div>
-        <div className="w-1/2">
-          <span className="flex items-center">
-            <span className="text-white p-2">User: </span>
-            <span className="text-white">asd</span>
-          </span>
+        <div className="w-[500px] h-[500px] border rounded-3xl ml-5 p-4">
+          <div className="">
+            <button
+              className="p-2 border-2 rounded-xl bg-white mr-5"
+              onClick={startListening}
+              disabled={isListening}
+            >
+              Start Listening
+            </button>
+            <button
+              className="p-2 border-2 rounded-xl bg-white "
+              onClick={stopListening}
+              disabled={!isListening}
+            >
+              Stop Listening
+            </button>
+          </div>
+          <div className="text-white flex flex-col">
+            <span className="font-bold">You</span>
+            <span>{text}</span>
+
+            <span className="font-bold mt-5">Luna</span>
+            <span>Hello there</span>
+
+          </div>
         </div>
       </div>
     </div>
